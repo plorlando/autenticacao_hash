ccdb31253798087e9f757c4eaf49ef2f7947e5f5
Pedro Lor Orlando <48284191+plorlando@users.noreply.github.com>
2023-11-11 01:15:11 +0000

    20231110

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import sys
 import os
+import hashlib
 
 arquivo = 'users.txt'
+
+if not os.path.isfile(arquivo):
+    with open(arquivo, 'w') as a:
+        a.write("usuario,senha")
 
 def exibir_menu():
     print("Menu:")
@@ -9,51 +14,85 @@ def exibir_menu():
     print("2. Autenticar Usuário")
     print("3. Sair")
 
-def verificar_usuario(arquivo_usuarios, usuario_atual):
-    for i in arquivo_usuarios:
+def verificar_usuario(a, usuario_atual):
+    for i in a:
         linha = i.split(',')
         usuario = linha[0]
         senha = linha[1]
         if usuario == usuario_atual:
             return True
-        else:
-            return False
+        
+    return False
     
 
-def cadastrar_usuario(arquivo_usuarios):
-    usuario_existe = verificar_usuario(arquivo_usuarios)
-    
-    pass
+def cadastrar_usuario(usuario_atual):
+    with open(arquivo, 'r+') as a:
+        usuario_existe = verificar_usuario(a, usuario_atual)
+
+        if usuario_existe:
+            print("Falha ao cadastrar. Já existe um usuário com esse nome.")
+        else:
+            senha_confirma, senha = confirma_senha()
+            senha_hashed = hashlib.md5(senha.encode())
+            print(senha_hashed)
+
+            if senha_confirma:
+                a.write(f"\n{usuario_atual.rstrip().lstrip()},{senha.rstrip().lstrip()}")
+                a.flush()
+                input("Usuário cadastrado com sucesso. Pressione qualquer tecla para continuar...")
+            else:
+                input("As senhas não conferem! Pressione qualquer tecla para continuar...")
+            
+        
+def confirma_senha():
+
+    senha_atual = input("Insira uma senha com pelo menos 8 digitos: ")
+    confirma_senha = input("Confirme a senha: ")
+
+    if senha_atual == confirma_senha:
+        return True, senha_atual
+    else:
+        return False, None
+
 
 def autenticar_usuario():
-    # Lógica para exibir as tarefas existentes
-    print("Tarefas existentes:")
-    print("- Tarefa 1")
-    print("- Tarefa 2")
+    usuario_atual = input("Usuário: ")
+    senha_atual = input("Senha: ")
 
+    with open(arquivo, 'r+') as a:
+        for i in a:
+            
+            linha = i.split(',')
+            usuario = linha[0]
+            senha = linha[1]
+            # print(f"USUARIO: {usuario} USUARIO ATUAL: {usuario_atual} SENHA: {senha} SENHA ATUAL: {senha_atual}")
+
+            if usuario == usuario_atual and senha == senha_atual:
+                return True, usuario_atual, senha_atual
+            
+        return False, usuario_atual, senha_atual
 
 
 def main():
-    
-    try:
-        arquivo_usuarios = open(arquivo, 'r+')
-        print("Aberto arquivo de usuários")
-    except FileNotFoundError:
-        arquivo_usuarios = open(arquivo, 'w+')
-        print("Criado arquivo de usários")
-    
+        
     while True:
         exibir_menu()
         
         opcao = input("Escolha uma opção: ")
         
         if opcao == "1":
-            usuario_atual = 'pedro'
-            cadastrar_usuario(arquivo_usuarios, usuario_atual)
+            usuario_atual = input("Digite um nome para seu usuário: ")
+            cadastrar_usuario(usuario_atual)
         elif opcao == "2":
-            autenticar_usuario()
+            autenticado, usuario_atual, senha_atual = autenticar_usuario()
+
+            if autenticado:
+                input("Usuário autenticado com sucesso. Pressione qualquer tecla para continuar...")
+            else:
+                input("Falha na autenticação. Usuário ou senha incorretos. Pressione qualquer tecla para continuar...")
+
+
         elif opcao == "3":
-            arquivo_usuarios.close()
             sys.exit()
         else:
             print("Opção inválida. Tente novamente.")
